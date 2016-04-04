ff55bacac8ba0869ca9a7e3f1b12cbaee0a8f0e0
Nate Dalo <nate.dalo@yahoo.com>
2016-04-04 12:47:44 -0500

    add ability to edit products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,19 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit
+    end
+  end
+
   def show
     @product = Product.find(params[:id])
   end
