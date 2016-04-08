4d5090ec05b8d04fa8529b993df54b6a946cc263
Nate Dalo <nate.dalo@yahoo.com>
2016-04-08 09:53:44 -0500

    remove old controller actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -58,49 +58,10 @@ class ProductsController < ApplicationController
   def reviews
   end
 
-  def black_touchscreen_glove
-  end
-
-  def white_touchscreen_glove
-  end
-
   def colored_touchscreen_glove
   end
 
-  def record_breaker
-  end
-
-  def record_staller
-  end
-
-  def round_pop
-  end
-
-  def spiral
-  end
-  
-  def round_stall
-  end
-
-  def flashlight
-  end
-
-  def dog_leash
-  end
-
   def itsridic_tshirt
-  end
-
-  def drifit_sock
-  end
-
-  def crystal_monkey
-  end
-
-  def headband
-  end
-
-  def led_dog_collar
   end
 
   private
